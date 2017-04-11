680746ffcd177e3af79e3508352ea87236c379a1
Skye <skye@m15.cn>
2017-04-11 16:29:51 +0800

    add mvp framework

---
+++ app/src/main/java/swift/com/camera/MainContract.java
@@ -0,0 +1,20 @@
+package swift.com.camera;
+
+/**
+ * Created by skye on 4/11/17.
+ */
+
+public class MainContract {
+
+    interface View {
+
+    }
+
+    interface Presenter {
+
+    }
+
+    interface Support {
+
+    }
+}
